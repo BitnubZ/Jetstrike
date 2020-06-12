4040feac7390f69db2e85812cbaea6a128cfb20b
BitnubZ <56090011+BitnubZ@users.noreply.github.com>
2020-06-12 16:53:35 +0100

    Add files via upload

---
+++ lineCt.py
@@ -0,0 +1,13 @@
+from glob import glob
+files = glob("*.py") + glob("Game/*.py")
+
+lines = 0
+comments = 0
+chars = 0
+
+for f in files: 
+	thing = open(f).read()
+	lines += thing.count("\n")
+	comments += thing.count("#")
+
+print("%d Lines\n%d Files\nWithout comments %d"%(lines, len(files), lines-comments))
